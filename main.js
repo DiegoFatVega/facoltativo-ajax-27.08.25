dbcbfa8520c4a5fced06c0463510fd02517cf54b
DiegoFatVega <fatvegadiego@gmail.com>
2025-08-30 16:40:30 +0200

    created card using names of the character of the film star-wars

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 console.log('Hello World')
 
 const swapiPersons = "https://swapi.info/api/people"; // --->endpoint salvato in una variabile
-const personListElements = document.getElementById('person')
+const cardListElements = document.getElementById('cards');
+
 fetch(swapiPersons)
     .then((res) => res.json())
     .then(element => {
@@ -9,12 +10,24 @@ fetch(swapiPersons)
              const thisElement = element[i];
              console.log(thisElement.title)
          } */
+        /*    element.forEach(person => {
+               console.log(person.name)
+               const liEl = document.createElement('li');
+               liEl.innerText = person.name;
+               personListElements.appendChild(liEl)
+           }) */
         element.forEach(person => {
-            console.log(person.name)
-            const liEl = document.createElement('li');
-            liEl.innerText = person.name;
-            personListElements.appendChild(liEl)
+            const cardString = `<div class="col mt-4">
+                                    <div class="card" style="width: 18rem;">
+                                     <img src="..." class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                               <h5 class="card-title">${person.name}</h5>
+                                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                                card’s content.</p>
+                                            </div>
+                                    </div>
+                                </div>`;
+    cardListElements.insertAdjacentHTML('beforeend', cardString);
         })
-
     })
     .catch((error) => console.error(error))
